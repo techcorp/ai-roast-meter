302c4d0030cef3a74f91266cdd266205ad68b853
Technical Corp Official <gophishtest74@gmail.com>
2025-12-03 17:00:35 +0500

    Uncomment index.css import for styling

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 // Uncomment the line below when building for production/deployment
-// import './index.css';
+import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
